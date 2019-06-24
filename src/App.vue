57abe69fd1ecadd9c53b06d0f0fdaa7011300f33
dobrien <dobrien@todopatterson.com>
2019-06-24 12:50:45 +0200

    lesson 5

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,17 @@
 <template>
   <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
+    <Navigation />
     <router-view />
   </div>
 </template>
-
+<script>
+import Navigation from '@/components/Navigation.vue'
+export default {
+    components: {
+      Navigation
+    }
+}
+</script>
 <style>
 #app {
   font-family: "Avenir", Helvetica, Arial, sans-serif;
@@ -26,6 +30,6 @@
 }
 
 #nav a.router-link-exact-active {
-  color: #42b983;
+  color: #ab26ab;
 }
 </style>
