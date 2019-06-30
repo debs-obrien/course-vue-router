c88d5310744bd96caa8c40525bbd5a7a2e6da718
dobrien <dobrien@todopatterson.com>
2019-06-30 16:54:57 +0200

    add route guards

--- src/router.js
+++ src/router.js
@@ -1,6 +1,7 @@
 import Vue from "vue";
 import Router from "vue-router";
 import Home from "./views/Home.vue";
+import store from "@/store.js";
 
 Vue.use(Router);
 
@@ -10,26 +11,36 @@ export default new Router({
     {
       path: "/",
       name: "home",
-      component: Home
+      component: Home,
+      props: true
     },
     {
       path: "/details/:location",
       name: "destinationDetails",
+      props: true,
       component: () =>
-        import(/* webpackChunkName: "DestinationDetails" */ "./views/DestinationDetails.vue")
+        import(/* webpackChunkName: "DestinationDetails" */ "./views/DestinationDetails.vue"),
+      beforeEnter: (to, from, next) => {
+        const slug = to.params.location;
+        if (store.destinations.find(destination => destination.slug === slug)) {
+          next();
+        } else {
+          next({ name: "notFound" });
+        }
+      }
     },
     {
       path: "/details/:location/:experience",
       name: "experienceDetails",
+      props: true,
       component: () =>
-        import(/* webpackChunkName: "ExperienceDetails" */ "./views/ExperienceDetails.vue")
+        import(/* webpackChunkName: "ExperienceDetails" */ "./views/ExperienceDetails.vue"),
     },
-
-{
+    {
       path: "*",
-      name: "Not Found",
+      name: "notFound",
       component: () =>
-        import(/* webpackChunkName: "Not Found" */ "./views/NotFound.vue")
+        import(/* webpackChunkName: "NotFound" */ "./views/NotFound.vue")
     }
   ]
 });
